5ddbb9fdbd1fa6f6bbce9259f461fd9c75df1b50
bloom510 <bloomjazzer@gmail.com>
2019-01-08 20:54:40 -0800

    fixing .gitignore

---
+++ crate/pkg/rust_webpack_bg.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function run(): void;
+export function greet(a: number, b: number): void;
+export function return_string(a: number): void;
+export function __wbindgen_global_argument_ptr(): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
